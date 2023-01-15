51dd077c13451dede6e8236e4241bb7de2d229eb
Gonzalo Gomez <gonzalogdv@gmail.com>
2023-01-15 20:01:20 -0300

    Favorites and Local Storage

--- src/Context.jsx
+++ src/Context.jsx
@@ -6,12 +6,23 @@ const AppContext = React.createContext();
 const allMealsUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
 const randomMealUrl = 'https://www.themealdb.com/api/json/v1/1/random.php';
 
+const getFavoritesFromLocalStorage = () => {
+  let favorites = localStorage.getItem('favorites');
+  if (favorites) {
+    favorites = JSON.parse(localStorage.getItem('favorites'));
+  } else {
+    favorites = [];
+  }
+  return favorites;
+};
+
 const AppProvider = ({ children }) => {
   const [loading, setLoading] = useState(false);
   const [meals, setMeals] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
   const [showModal, setShowModal] = useState(false);
   const [selectedMeal, setSelectedMeal] = useState(null);
+  const [favorites, setFavorites] = useState(getFavoritesFromLocalStorage());
 
   const fetchMeals = async (url) => {
     setLoading(true);
@@ -34,14 +45,32 @@ const AppProvider = ({ children }) => {
 
   const selectMeal = (idMeal, favoriteMeal) => {
     let meal;
-
-    meal = meals.find((meal) => meal.idMeal === idMeal);
+    if (favoriteMeal) {
+      meal = favorites.find((meal) => meal.idMeal === idMeal);
+    } else {
+      meal = meals.find((meal) => meal.idMeal === idMeal);
+    }
     setSelectedMeal(meal);
     setShowModal(true);
   };
 
   const closeModal = () => {
     setShowModal(false);
+  };
+
+  const addToFavorites = (idMeal) => {
+    const alreadyFavorites = favorites.find((meal) => meal.idMeal === idMeal);
+    if (alreadyFavorites) return;
+    const meal = meals.find((meal) => meal.idMeal === idMeal);
+    const updatedFavorites = [...favorites, meal];
+    setFavorites(updatedFavorites);
+    localStorage.setItem('favorites', JSON.stringify(updatedFavorites));
+  };
+
+  const removeFromFavorites = (idMeal) => {
+    const updatedFavorites = favorites.filter((meal) => meal.idMeal !== idMeal);
+    setFavorites(updatedFavorites);
+    localStorage.setItem('favorites', JSON.stringify(updatedFavorites));
   };
 
   useEffect(() => {
@@ -64,6 +93,9 @@ const AppProvider = ({ children }) => {
         selectedMeal,
         selectMeal,
         closeModal,
+        addToFavorites,
+        removeFromFavorites,
+        favorites,
       }}
     >
       {children}
